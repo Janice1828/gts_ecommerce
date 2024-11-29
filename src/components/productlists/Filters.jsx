9f8637a165eee21ea906d82424718177df79169f
Jenish Limbu <jlimbu539@gmail.com>
2024-11-30 00:11:39 +0545

    filters added

--- src/components/productlists/Filters.jsx
+++ src/components/productlists/Filters.jsx
@@ -1,13 +1,74 @@
-import React from 'react'
+import React, { useContext, useState, useEffect } from 'react'
+const Filters = ({ colors, brands, filteredProducts, updateProducts }) => {
+    const [selectedBrands, setSelectedBrands] = useState([])
+    const [selectedColors, setSelectedColors] = useState([])
+    const [filteredResults, setFilteredResults] = useState(filteredProducts);
+    const handleBrandFilter = (event) => {
+        if (event.target.checked) {
+            setSelectedBrands([...selectedBrands, event.target.value]);
+        } else {
+            setSelectedBrands(
+                selectedBrands.filter((brand) => brand !== event.target.value)
+            );
+        }
+    };
+    const handleColorFilter = (event) => {
+        if (event.target.checked) {
+            setSelectedColors([...selectedColors, event.target.value]);
+        } else {
+            setSelectedColors(
+                selectedColors.filter((color) => color !== event.target.value)
+            );
+        }
+    };
+    useEffect(() => {
+        let filterData = filteredProducts;
+        if (selectedBrands.length > 0) {
+            filterData = filterData.filter((item) =>
+                selectedBrands.includes(item.brand)
+            );
+        }
+        if (selectedColors.length > 0) {
+            filterData = filterData.filter((item) =>
+                selectedColors.includes(item.color)
+            );
+        }
+        updateProducts(filterData)
+    }, [selectedBrands, selectedColors, filteredProducts]);
 
-const Filters = () => {
     return (
         <div className='bg-white p-4'>
             <h5 className='fw-semibold normal-text-color'>Filters</h5>
             <p className='normal-text-color fw-semibold'>Brands</p>
-            <div className='d-flex gap-1 align-items-center'><input type="checkbox" /><label htmlFor="">Samsung</label></div>
+            <div className='d-flex flex-wrap gap-3'>
+                {brands.map((brand, key) => (
+                    <div key={key} className='d-flex gap-1 align-items-center'>
+                        <input
+                            type="checkbox"
+                            id={brand}
+                            value={brand}
+                            onChange={handleBrandFilter}
+                        />
+                        <label htmlFor={brand}>{brand}</label>
+                    </div>
+                ))}
+            </div>
+            <p className='normal-text-color fw-semibold mt-4 '>Colors</p>
+            <div className='d-flex flex-wrap gap-3'>
+                {colors.map((color, key) => (
+                    <div key={key} className='d-flex gap-1 align-items-center'>
+                        <input
+                            type="checkbox"
+                            id={color}
+                            value={color}
+                            onChange={handleColorFilter}
+                        />
+                        <label htmlFor={color}>{color}</label>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
 
-export default Filters
+export default Filters;
--- src/pages/Productlist.jsx
+++ src/pages/Productlist.jsx
@@ -1,24 +1,44 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import Navbar from '../components/global/Navbar'
 import Footer from '../components/global/Footer'
 import Filters from '../components/productlists/Filters'
 import Product from '../components/global/Product'
+import { useEffect } from 'react'
 import { ProductContext } from '../App'
 const Productlist = () => {
   const search = window.location.search;
   const params = new URLSearchParams(search);
   const category = params.get("category");
+  const [displayingCategory, setDisplayingCategory] = useState([])
   const products = useContext(ProductContext)
-  const displayingCategory = products.filter((product) => {
-    return product.category == category ? product : ''
+  useEffect(() => {
+    let newData = products.filter((product) => {
+      return product.category == category ? product : ''
+    })
+    setDisplayingCategory(newData)
+  }, [products])
+  const currentCategoriesProducts = products.filter((product) => {
+    return product.category == category ? product : '';
   })
+  const colors = [];
+  const brand = [];
+  currentCategoriesProducts.forEach((product) => {
+    colors.push(product.color);
+    brand.push(product.brand)
+  })
+  const uniqueColors = new Set(colors);
+  const uniqueBrands = new Set([...brand])
+  const uniqueColorsArr = [...uniqueColors]
+  const uniqueBrandsArr = [...uniqueBrands]
   return (
     <div>
       <Navbar />
       <div className="content">
         <div className="container">
           <div className="row py-5">
-            <div className="col-md-3 pb-md-0 pb-4"><Filters /></div>
+            <div className="col-md-3 pb-md-0 pb-4">
+              <Filters colors={uniqueColorsArr} brands={uniqueBrandsArr} filteredProducts={currentCategoriesProducts} updateProducts={setDisplayingCategory} />
+            </div>
             <div className="col-md-9">
               <div className='row w-100 gy-4'>
                 {displayingCategory.map((product, key) => (
